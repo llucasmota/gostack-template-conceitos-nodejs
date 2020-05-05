91c2dd98865fcfe327639c7387a94b5c57d24439
Lucas Mota <lucas.o.mota@gmail.com>
2020-05-05 09:35:50 -0300

    Disponibilizando rotas de Criação, atualização e consulta

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,16 +10,56 @@ app.use(cors());
 
 const repositories = [];
 
+function validProjectId(request, response, next) {
+  const { id } = request.params;
+  if (!isUuid(id)) {
+    return response
+      .status(401)
+      .json({ error: { message: "Invalid project id" } });
+  }
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  if (!title) {
+    return response
+      .status(400)
+      .json({ error: { message: "Informar todos os parâmetros" } });
+  }
+  if (!url) {
+    return response
+      .status(400)
+      .json({ error: { message: "Informar todos os parâmetros" } });
+  }
+  if (!techs) {
+    return response
+      .status(400)
+      .json({ error: { message: "Informar todos os parâmetros" } });
+  }
+  const repositorio = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repositorio);
+  return response.status(201).json(repositorio);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validProjectId, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const reporsitorieIndex = repositories.findIndex((repo) => repo.id === id);
+
+  if (reporsitorieIndex < 0) {
+    return response
+      .status(401)
+      .json({ error: { message: "Repositório não encontrado" } });
+  }
+  const repository = { id, title, url, techs };
+  Object.assign(repositories[reporsitorieIndex], repository);
+  return response.status(200).json(repositories[reporsitorieIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
